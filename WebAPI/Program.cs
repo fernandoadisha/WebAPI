66e1709fdcd8919e6b6fbcf95170b92a1cfbe29a
Adhisha Fernando <fernandoadisha007@gmail.com>
2022-08-26 14:20:39 +0530

    Advanced in more Get/Set parts

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,6 @@
 using Newtonsoft.Json.Serialization;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +36,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+    RequestPath="/Photos"
+}) ;
 
 app.UseHttpsRedirection();
 
